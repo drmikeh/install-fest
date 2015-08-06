79ba8ef0e494a6150c83661afb986b8f2c0bda93
Mike Hopper <drmikeh@gmail.com>
2015-08-06 18:15:46 -0400

    Fixed send_mail

--- lib/send_mail.sh
+++ lib/send_mail.sh
@@ -4,5 +4,5 @@ send_mail() {
   TO_ADDRESS=$1
   SUBJECT=$2
   log_info "\n=== Emailing install-fest to $TO_ADDRESS ==="
-  mail -s ${SUBJECT} ${TO_ADDRESS} < $LOGFILE
+  mail -s "${SUBJECT}" "${TO_ADDRESS}" < $LOGFILE
 }
