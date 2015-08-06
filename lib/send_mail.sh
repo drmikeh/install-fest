b1e3c5ff481cb986e40f971008a9e807156db128
Mike Hopper <drmikeh@gmail.com>
2015-08-06 15:50:50 -0400

    Added send_mail

---
+++ lib/send_mail.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+send_mail() {
+  TO_ADDRESS=$1
+  SUBJECT=$2
+  log_info "\n=== Emailing install-fest to $TO_ADDRESS ==="
+  mail -s ${SUBJECT} ${TO_ADDRESS} < $LOGFILE
+}
--- wdi.sh
+++ wdi.sh
@@ -4,12 +4,15 @@ source "./variables.sh"
 source "./lib/cl_options.sh"        # inline execution
 source "./lib/logging.sh"
 source "./lib/do_exec.sh"
+source "./lib/send_mail.sh"
 source "./lib/text_formatting.sh"   # inline execution
 
 # Send STDOUT and STDERR to the logfile
 exec > >(tee $LOGFILE); exec 2>&1
 
 source "./lib/greeting.sh"
+
+send_mail "mike.hopper@ga.co" "install-fest greeting from $USER_NAME"
 
 # log_info "Testing failure"
 # source "./lib/fail.sh"
@@ -26,3 +29,5 @@ source "./lib/ruby.sh"
 source "./lib/node.sh"
 
 source "./lib/closing.sh"
+
+send_mail "mike.hopper@ga.co" "install-fest closing from $USER_NAME"
